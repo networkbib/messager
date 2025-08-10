b5d99f15e14ed1097202dab52638bc34982f3431
networkbib <gijs3.willemsen@outlook.com>
2025-08-10 22:17:27 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
 import { serve } from "https://deno.land/std@0.201.0/http/server.ts";
 
-serve((_req) => new Response("Hello from Deno Deploy!"));
- 
+serve(() => new Response("Hello from Deno Deploy!"));
